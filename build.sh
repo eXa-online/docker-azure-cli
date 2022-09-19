414d300c0f3e4b640bd9530fe786eea0757e4a4b
René Muhl <rmuhl@exa-online.de>
2022-09-19 15:46:42 +0200

    Use long parameter names

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker buildx create --name mybuilder --driver docker-container --bootstrap --use
-docker buildx build --push --platform linux/amd64,linux/arm64 -t exaonline/azure-cli:1.0.0 -t exaonline/azure-cli:latest .
+docker buildx build --push --platform linux/amd64,linux/arm64 --tag exaonline/azure-cli:1.0.0 --tag exaonline/azure-cli:latest .
