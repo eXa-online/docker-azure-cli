f870b57b24f9d5a221973195a5d6e37f33d6bf8c
René Muhl <rmuhl@exa-online.de>
2022-09-21 15:52:50 +0200

    Fix multi-platform build

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-docker buildx create --name multi-platform-builder --driver docker-container --bootstrap --use
-docker buildx build --push --platform linux/amd64,linux/arm64 --tag exaonline/azure-cli:1.0.0 --tag exaonline/azure-cli:latest .
+VERSION=1.0.0
+
+docker buildx create --use
+docker buildx build --push \
+  --platform linux/amd64,linux/arm64 \
+  --tag exaonline/azure-cli:"$VERSION" \
+  --tag exaonline/azure-cli:latest .
